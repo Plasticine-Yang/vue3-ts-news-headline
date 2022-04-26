5674a4c0c1f170e4db10d9c03ce96fbfa0391a29
plasticine9750 <975036719@qq.com>
2022-04-26 15:11:44 +0800

    style(Detail): modify news detail iframe style

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe v-if="newsData" class="min-h-[100vh]" :src="newsData.url" />
+  <iframe v-if="newsData" class="min-h-[100vh] w-full" :src="newsData.url" />
   <div v-else class="flex justify-center items-center min-h-[100vh]">
     <p class="text-[#aeaeae] text-2xl">新闻不存在</p>
   </div>
